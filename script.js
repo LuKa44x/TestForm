68256b98d641c70b522a548c24a7ea6a2a64f617
LuKa44x <lucavicari44@gmail.com>
2025-05-02 12:22:55 +0200

    trying google for server side

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+document.getElementById("myForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const data = {
+    nome: e.target.nome.value,
+    email: e.target.email.value,
+    messaggio: e.target.messaggio.value
+  };
+
+  fetch("https://script.google.com/macros/s/AKfycbxZCq2d8nyPdzH0CnDRNN6LRgMtfY9p9xikCpR9Pns6_dqpoHEnmQut2gxxkXkzMxtn2g/exec", {
+    method: "POST",
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(response => {
+    alert("Messaggio inviato con successo!");
+  })
+  .catch(error => {
+    alert("Errore nell'invio!");
+    console.error(error);
+  });
+});
+
+
+
+
+
+
+
 const range = document.getElementById('range');
   const valore = document.getElementById('range-value');
 
