e76141b321f325a86b659203d0f25d37a3594664
LuKa44x <lucavicari44@gmail.com>
2025-05-02 12:32:02 +0200

    fix

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ document.getElementById("myForm").addEventListener("submit", function(e) {
   const data = {
     nome: e.target.nome.value,
     email: e.target.email.value,
-    messaggio: e.target.messaggio.value
+    password: e.target.password.value,
+    bDay: e.target.bDay.value,
+    phone: e.target.phone.value,
+    State: e.target.State.value,
+    City: e.target.City.value,
+    Address: e.target.Address.value,
+    message: e.target.message.value,
+    file: e.target.file.value,
+    color: e.target.color.value,
+    Hate: e.target.Hate.value,
+    range: e.target.range.value,
+    food: e.target.food.value,
+    checkbox: e.target.checkbox.checked
   };
 
   fetch("https://script.google.com/macros/s/AKfycbxZCq2d8nyPdzH0CnDRNN6LRgMtfY9p9xikCpR9Pns6_dqpoHEnmQut2gxxkXkzMxtn2g/exec", {
